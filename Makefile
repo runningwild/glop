f098da45eeed7a0fb13268f7645ec81c0d4eab82
Jonathan Wills <runningwild@gmail.com>
2011-08-06 22:53:28 -0400

    Updated some osx stuff

--- Makefile
+++ Makefile
@@ -3,8 +3,7 @@ include $(GOROOT)/src/Make.inc
 TARG=glop
 
 CGOFILES=glop.go
-
-CGO_LDFLAGS=-lglop -framework Cocoa -framework OpenGL
+CGO_LDFLAGS=-lglop -framework Cocoa -framework OpenGL -mmacosx-version-min=10.4
 
 include $(GOROOT)/src/Make.pkg
 
--- make.bash
+++ make.bash
@@ -3,8 +3,8 @@ cp glop.h /usr/local/include/glop.h
 
 mkdir -p obj_glop
 mkdir -p lib
-gcc -arch x86_64 -m64 -fPIC -c -o obj_glop/glop_m.o glop.m
-gcc -shared -W1 -o lib/libglop.so obj_glop/glop_m.o  -framework Cocoa -framework OpenGL
+gcc -mmacosx-version-min=10.5 -arch x86_64 -m64 -fPIC -c -o obj_glop/glop_m.o glop.m
+gcc -mmacosx-version-min=10.5 -shared -W1 -o lib/libglop.so obj_glop/glop_m.o  -framework Cocoa -framework OpenGL
 cp lib/libglop.so /usr/local/lib/libglop.so
 
 make clean
