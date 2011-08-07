d060e4e8109af45d507921aabfb640e42157ffd2
Jonathan Wills <runningwild@gmail.com>
2011-08-07 19:47:50 -0400

    separating carbon from cocoa

---
+++ include/glop.h
@@ -0,0 +1,12 @@
+#ifndef __GLOP_H__
+#define __GLOP_H__
+
+void Init();
+void CreateWindow(void**, void**, int, int, int, int);
+void Run();
+void SwapBuffers(void*);
+void Think();
+
+void CurrentMousePos(void*,void*,void*);
+
+#endif
