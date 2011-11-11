8057d34251c6a8dd5e66c8051b12d9277ba0616c
Jonathan Wills <runningwild@gmail.com>
2011-11-11 16:53:02 -0500

    Shouldn't have checked in those printfs

--- gos/darwin/glop.m
+++ gos/darwin/glop.m
@@ -332,7 +332,6 @@ int* getInputStateVal(int flag) {
   do {
 //    [pool release];
 //    pool = [[NSAutoreleasePool alloc] init];
-printf("run1\n");
     NSEvent *event =
       [self
         nextEventMatchingMask:NSAnyEventMask
@@ -344,12 +343,8 @@ printf("run1\n");
     if (!on_correct_thread) {
       return;
     }
-    printf("%d\n", [event type]);
-printf("run2\n");
     [self sendEvent:event];
-printf("run3\n");
     [self updateWindows];
-printf("run4\n");
   } while (!should_stop);
   should_stop = 0;
 //  [pool release];
@@ -363,23 +358,17 @@ void Quit() {
 int Think() {
   // TODO: This is retarded, but it does seem to get all of the evnts out of the queue
   // rather than only most of them
-  printf("A\n");
   [glop_app postEvent:terminator atStart:FALSE];
-  printf("B\n");
   [glop_app run];
   if (![(GlopApplication*)glop_app onCorrectThread]) {
     return 0;
   }
-  printf("C\n");
   [glop_app postEvent:terminator atStart:FALSE];
-  printf("D\n");
   [glop_app run];
   if (![(GlopApplication*)glop_app onCorrectThread]) {
     return 0;
   }
-  printf("E\n");
   osx_horizon = [[NSProcessInfo processInfo] systemUptime];
-  printf("F\n");
   return 1;
 }
 
