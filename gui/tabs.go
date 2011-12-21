d8b132d7b7c9c112025e721aabd15e4b390661b8
Jonathan Wills <runningwild@gmail.com>
2011-12-21 00:15:49 -0800

    Basic tabs widget - has some bugs involving focus

---
+++ gui/tabs.go
@@ -0,0 +1,55 @@
+package gui
+
+type TabFrame struct {
+  NonFocuser
+  BasicZone
+  StandardParent
+  active int
+}
+
+func MakeTabFrame(ws []Widget) *TabFrame {
+  var frame TabFrame
+  for _,w := range ws {
+    frame.Children = append(frame.Children, w)
+  }
+  return &frame
+}
+
+func (w *TabFrame) String() string {
+  return "tab frame"
+}
+
+func (w *TabFrame) SelectTab(n int) {
+  if n >= 0 && n < len(w.Children) {
+    w.active = n
+  }
+}
+
+func (w *TabFrame) SelectedTab() int {
+  return w.active
+}
+
+func (w *TabFrame) Respond(gui *Gui, group EventGroup) bool {
+  cursor := group.Events[0].Key.Cursor()
+  if cursor != nil {
+    var p Point
+    p.X, p.Y = cursor.Point()
+    if !p.Inside(w.Rendered()) {
+      return false
+    }
+  }
+  return w.Children[w.active].Respond(gui, group)
+}
+
+func (w *TabFrame) Think(gui *Gui, t int64) {
+  for i := range w.Children {
+    w.Children[i].Think(gui, t)
+  }
+  w.Request_dims = w.Children[w.active].Requested()
+}
+
+func (w *TabFrame) Draw(region Region) {
+  tab := w.Children[w.active]
+  tab.Draw(region)
+  w.Render_region = tab.Rendered()
+}
