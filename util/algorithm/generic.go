4a97a272b7342bb15f038ad6b3e53cf72ad94436
Jonathan Wills <runningwild@gmail.com>
2012-10-16 13:30:29 -0700

    Finally moved Choose2 -> Choose and Map2 -> Map

--- util/algorithm/generic.go
+++ util/algorithm/generic.go
@@ -7,37 +7,11 @@ import (
 
 type Chooser func(interface{}) bool
 
-// Given a slice and a Chooser, returns a slice of the same type as the input
-// slice that contains only those elements of the input slice for which
-// choose() returns true.  The elements of the returned slice will be in the
-// same order that they were in in the input slice.
-func Choose(_a interface{}, choose Chooser) interface{} {
-  a := reflect.ValueOf(_a)
-  if a.Kind() != reflect.Slice {
-    panic(fmt.Sprintf("Can only Choose from a slice, not a %v", a))
-  }
-  count := 0
-  for i := 0; i < a.Len(); i++ {
-    if choose(a.Index(i).Interface()) {
-      count++
-    }
-  }
-  ret := reflect.MakeSlice(a.Type(), count, count)
-  cur := 0
-  for i := 0; i < a.Len(); i++ {
-    if choose(a.Index(i).Interface()) {
-      ret.Index(cur).Set(a.Index(i))
-      cur++
-    }
-  }
-  return ret.Interface()
-}
-
 // //  Given a pointer to a slice and a Chooser,  type as the input
 // // slice that contains only those elements of the input slice for which
 // // choose() returns true.  The elements of the returned slice will be in the
 // // same order that they were in in the input slice.
-func Choose2(_a interface{}, chooser interface{}) {
+func Choose(_a interface{}, chooser interface{}) {
   a := reflect.ValueOf(_a)
   if a.Kind() != reflect.Ptr || a.Elem().Kind() != reflect.Slice {
     panic(fmt.Sprintf("Can only Choose from a pointer to a slice, not a %v", a))
@@ -66,36 +40,16 @@ func Choose2(_a interface{}, chooser interface{}) {
     out = c.Call(in)
     if out[0].Bool() {
       if count > 0 {
-        slice.Index(i-count).Set(slice.Index(i))
+        slice.Index(i - count).Set(slice.Index(i))
       }
     } else {
       count++
     }
   }
-  slice.Set(slice.Slice(0, slice.Len() - count))
+  slice.Set(slice.Slice(0, slice.Len()-count))
 }
 
 type Mapper func(a interface{}) interface{}
-
-func Map(_a interface{}, _b interface{}, mapper Mapper) interface{} {
-  a := reflect.ValueOf(_a)
-  if a.Kind() != reflect.Slice {
-    panic(fmt.Sprintf("Can only Map from a slice, not a %v", a))
-  }
-
-  b := reflect.ValueOf(_b)
-  if b.Kind() != reflect.Slice {
-    panic(fmt.Sprintf("Can only Map to a slice, not a %v", b))
-  }
-
-  ret := reflect.MakeSlice(b.Type(), a.Len(), a.Len())
-  for i := 0; i < a.Len(); i++ {
-    el := reflect.ValueOf(mapper(a.Index(i).Interface()))
-    ret.Index(i).Set(el)
-  }
-
-  return ret.Interface()
-}
 
 func Map2(_in interface{}, _out interface{}, mapper interface{}) {
   in := reflect.ValueOf(_in)
@@ -136,11 +90,3 @@ func Map2(_in interface{}, _out interface{}, mapper interface{}) {
     out.Elem().Index(i).Set(v[0])
   }
 }
-
-
-
-
-
-
-
-
